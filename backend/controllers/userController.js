eda7f45a3e41113cbdee8c7bd9403de4da940c35
Olaniyi Sunday Emmanuel <Olaniyisunday323@gmail.com>
2023-03-10 01:35:30 -0800

    Protect routes and authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,6 +70,18 @@ const loginUser = asyncHandler(async  (req, res) => {
   }
 })
 
+// @desc Get current user
+// @route /api/users/me
+// @acess Private
+const getMe = asyncHandler(async  (req, res) => {
+  const {_id, name, email} = req.user
+  res.status(200).json({
+    id: _id,
+    name,
+    email
+  })
+})
+
 // Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -79,5 +91,6 @@ const generateToken = (id) => {
 
 module.exports = {
   registerUser,
-  loginUser
+  loginUser,
+  getMe
 }
