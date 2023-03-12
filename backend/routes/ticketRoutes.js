0dc81e7a46b1d17406ef850c8003b1a6fc6c5ea5
Olaniyi Sunday Emmanuel <Olaniyisunday323@gmail.com>
2023-03-12 05:19:10 -0700

    Single Ticket Update and Delete (Backend)

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {getTickets, createTicket} = require('../controllers/ticketController')
+const {getTickets, getTicket, createTicket, deleteTicket, updateTicket} = require('../controllers/ticketController')
 
 const {protect} = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getTickets).post(protect, createTicket)
+
+router.route('/:id').get(protect, getTicket).put(protect, updateTicket).delete(protect, deleteTicket)
 
 module.exports = router
